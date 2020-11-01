e99efde1c615037ac2ef1cf560947a3467aa766e
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-11-01 17:27:13 +0100

    Add resolution variables for an easier customization

--- overridemouse.sh
+++ overridemouse.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+xlimit=1366
+yleft=768
+yright=1920
+sensitivity=15
+
 while [ true ]
 	do
 		eval $(xdotool getmouselocation --shell);
-		if [ $X -ge 1367 ] && [ $X -le 1377 ]
+		if [ $X -ge $((xlimit+1)) ] && [ $X -le $((xlimit+sensitivity+1)) ]
 			then
-				((NEWY=Y*768/1920+1151));
-				xdotool mousemove 1355 $NEWY;
+				((NEWY=Y*yleft/yright+((yright-yleft))));
+				xdotool mousemove $((xlimit-sensitivity-1)) $NEWY;
 		fi
 
-		if [ $X -le 1366 ] && [ $X -ge 1356 ]
+		if [ $X -le $((xlimit)) ] && [ $X -ge $((xlimit-sensitivity)) ]
 			then
-				((NEWY=((Y-1151))*1920/768));
-				xdotool mousemove 1378 $NEWY;
+				((NEWY=((Y-yright+yleft))*yright/yleft));
+				xdotool mousemove $((xlimit+sensitivity+2)) $NEWY;
 		fi
 	done
